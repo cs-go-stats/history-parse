18581903f95caa086eb1bc59352432a3787e8601
Viktor Liutianskyi <born.to.run.unix@gmail.com>
2020-01-06 10:12:02 +0200

    Fixed startup issues related to Core functionality.

--- history-parse/Scheduling/ScheduleExtensions.cs
+++ history-parse/Scheduling/ScheduleExtensions.cs
@@ -8,14 +8,12 @@ namespace CSGOStats.Services.HistoryParse.Scheduling
 {
     public static class ScheduleExtensions
     {
-        public static async Task ConfigureJobsAsync(
+        public static Task ConfigureJobsAsync(
             IScheduler scheduler,
             IServiceProvider serviceProvider,
-            IConfigurationRoot configuration)
-        {
-            await scheduler.ScheduleJob(
-                jobDetail: serviceProvider.CreateJobTemplate<DefaultJob>(),
-                trigger: SchedulerExtensions.CreateCronScheduledTriggerFromConfiguration(configuration, "Jobs:Default:CronExpression"));
-        }
+            IConfigurationRoot configuration) =>
+                scheduler.ScheduleJob(
+                    jobDetail: serviceProvider.CreateJobTemplate<DefaultJob>(),
+                    trigger: SchedulerExtensions.CreateCronScheduledTriggerFromConfiguration(configuration, "Jobs:Default:CronExpression"));
     }
 }
